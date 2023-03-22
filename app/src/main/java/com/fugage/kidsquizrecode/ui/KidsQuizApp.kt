93dcf6ec0c1764392a820d19d1502c9d32063ccb
gayeon00 <chlrkdus4819@gmail.com>
2023-03-23 01:03:11 +0900

    Initial commit

---
+++ app/src/main/java/com/fugage/kidsquizrecode/ui/KidsQuizApp.kt
@@ -0,0 +1,51 @@
+package com.fugage.kidsquizrecode.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import com.fugage.kidsquizrecode.ui.theme.KidsQuizRecodeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KidsQuizApp(){
+    KidsQuizRecodeTheme {
+        Scaffold(
+            bottomBar = { KidsQuizBottomNavigation() }
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Greeting("Android")
+            }
+        }
+/*        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Greeting("Android")
+        }*/
+    }
+}
+
+@Composable
+fun KidsQuizBottomNavigation(){
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
