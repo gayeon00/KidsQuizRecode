aaf7177a079dcd08ce58e6e56d3862d5f30952d8
gayeon00 <chlrkdus4819@gmail.com>
2023-06-13 22:13:58 +0900

    refactor: 폰트 추가

--- app/src/main/java/com/fugage/kidsquizrecode/ui/theme/Type.kt
+++ app/src/main/java/com/fugage/kidsquizrecode/ui/theme/Type.kt
@@ -2,18 +2,45 @@ package com.fugage.kidsquizrecode.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.fugage.kidsquizrecode.R
+
+val NotoSansKR = FontFamily(
+    Font(R.font.notosanskr_regular),
+    Font(R.font.notosanskr_bold, FontWeight.Bold),
+    Font(R.font.notosanskr_light, FontWeight.Light),
+    Font(R.font.notosanskr_medium, FontWeight.Medium),
+    Font(R.font.notosanskr_thin, FontWeight.Thin)
+)
+
+val GasoekOne = FontFamily(
+    Font(R.font.gasoekone_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = GasoekOne,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = NotoSansKR,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -32,3 +59,4 @@ val Typography = Typography(
     )
     */
 )
+
