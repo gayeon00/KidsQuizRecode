d26801bb20ee6e8d69b839cf497ebb306d5bd293
gayeon00 <chlrkdus4819@gmail.com>
2023-06-13 23:09:45 +0900

    refactor: HomeScreen 수정
    - 폰트 적용
    - 중앙 정렬
    - Card이용

--- app/src/main/java/com/fugage/kidsquizrecode/ui/home/HomeScreen.kt
+++ app/src/main/java/com/fugage/kidsquizrecode/ui/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.fugage.kidsquizrecode.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -19,8 +23,7 @@ import com.chargemap.compose.numberpicker.FullHours
 import com.chargemap.compose.numberpicker.Hours
 import com.chargemap.compose.numberpicker.HoursNumberPicker
 import com.chargemap.compose.numberpicker.NumberPicker
-import com.fugage.kidsquizrecode.ui.theme.md_theme_dark_onBackground
-import com.fugage.kidsquizrecode.ui.theme.md_theme_dark_onSurface
+import com.fugage.kidsquizrecode.R
 import com.fugage.kidsquizrecode.ui.theme.md_theme_light_onSurface
 
 @Composable
@@ -35,35 +38,51 @@ fun HomeScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
             .fillMaxWidth()
-            .padding(10.dp)
+            .padding(PaddingValues(10.dp, 0.dp))
     ) {
-        Text(text = "우리 아이는 몇 살 인가요?")
+        Text(
+            text = stringResource(R.string.ask_child_age),
+            style = MaterialTheme.typography.displayMedium
+        )
 
-        Card(modifier = modifier) {
-            NumberPicker(
-                value = 2,
-                onValueChange = {
-                    agePickerValue = it
-                },
-                range = 2..6,
-                dividersColor = md_theme_light_onSurface,
-            )
+        Card(modifier = modifier
+        ) {
+            Row(
+                modifier = modifier.padding(PaddingValues(10.dp, 0.dp)),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                NumberPicker(
+                    value = agePickerValue,
+                    onValueChange = {
+                        agePickerValue = it
+                    },
+                    range = 2..6,
+                    dividersColor = md_theme_light_onSurface,
+                )
+                Text(text = stringResource(R.string.age_old))
+            }
+
         }
 
-        Text(text = "몇 분에 한 번씩 퀴즈를 풀까요?")
-        Card(modifier = modifier) {
+        Text(
+            text = stringResource(R.string.ask_time_interval),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Card(modifier = modifier
+        ) {
             HoursNumberPicker(
+                modifier = modifier.padding(PaddingValues(10.dp, 0.dp)),
                 value = fullHourPickerValue,
                 onValueChange = { fullHourPickerValue = it },
                 hoursDivider = {
                     Text(
-                        text = "분"
+                        text = stringResource(R.string.minute)
                     )
                 },
                 hoursRange = 0..59,
                 minutesDivider = {
                     Text(
-                        text = "초"
+                        text = stringResource(R.string.second)
                     )
                 },
                 minutesRange = 0..59,
